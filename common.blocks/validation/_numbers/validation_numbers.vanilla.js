0f13dd1b1bce90210b9f790df23a924dc1b01e86
Alexej Yaroshevich <alex@qfox.ru>
2015-01-05 16:50:31 +0400

    validation_numbers: added min/max params

--- common.blocks/validation/_numbers/validation_numbers.vanilla.js
+++ common.blocks/validation/_numbers/validation_numbers.vanilla.js
@@ -7,9 +7,37 @@ modules.define('validation_numbers',
 
     var NUMBERS_RE = /^\d+(\.\d+)?$/;
 
-    provide(function (message) {
+    provide(function (message, params) {
+        message = message || 'Should be a number (e.g. 123.45)';
+        if(params && params.min) {
+            params.min = Number(params.min);
+        }
+        if(params && params.max) {
+            params.max = Number(params.max);
+        }
+
         return function (val) {
-            return !val || NUMBERS_RE.test(val)? null : message || 'Should be a number (e.g. 123.45)';
+            if(!val) {
+                return null;
+            }
+
+            if(!NUMBERS_RE.test(val)) {
+                return message;
+            }
+
+            if(!params) {
+                return null;
+            }
+
+            val = Number(val);
+            if(params.min && val < params.min) {
+                return message;
+            }
+            if(params.max && val > params.max) {
+                return message;
+            }
+
+            return null;
         };
     });
 
